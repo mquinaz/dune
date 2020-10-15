99d23ec09004a4cf4f7f7b63aa9463aedd911658
jcosta <jcosta@lsts.pt>
2020-10-15 14:21:22 +0100

    Monitors/Activatable: created task (Exercise 3).

---
+++ src/Monitors/Activatable/Task.cpp
@@ -0,0 +1,138 @@
+//***************************************************************************
+// Copyright 2007-2020 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Faculdade de Engenharia da             *
+// Universidade do Porto. For licensing terms, conditions, and further      *
+// information contact lsts@fe.up.pt.                                       *
+//                                                                          *
+// Modified European Union Public Licence - EUPL v.1.1 Usage                *
+// Alternatively, this file may be used under the terms of the Modified     *
+// EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Joao Costa                                                       *
+//***************************************************************************
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+namespace Monitors
+{
+  //! Insert short task description here.
+  //!
+  //! Insert explanation on task behaviour here.
+  //! @author Joao Costa
+  namespace Activatable
+  {
+    using DUNE_NAMESPACES;
+
+    struct Arguments
+    {
+      //! max number of messages to be printed before deactivation
+      int limit;
+    };
+
+    struct Task : public DUNE::Tasks::Task
+    {
+      //! Announce message counter.
+      int m_counter;
+      //! Task arguments.
+      Arguments m_args;
+
+      //! Constructor.
+      //! @param[in] name task name.
+      //! @param[in] ctx context.
+      Task(const std::string &name, Tasks::Context &ctx):
+       DUNE::Tasks::Task(name, ctx),
+       m_counter(0)
+      {
+        paramActive(Tasks::Parameter::SCOPE_IDLE,
+                    Tasks::Parameter::VISIBILITY_USER,
+                    true);
+
+        param("Messages Number", m_args.limit)
+            .defaultValue("5")
+            .description("Announces to Print");
+
+        bind<IMC::Announce>(this);
+      }
+
+      void
+      consume(const IMC::Announce *msg)
+      {
+        m_counter++;
+      }
+
+      //! Update internal state with new parameter values.
+      void
+      onUpdateParameters(void)
+      {
+      }
+
+      //! Reserve entity identifiers.
+      void
+      onEntityReservation(void)
+      {
+      }
+
+      //! Resolve entity names.
+      void
+      onEntityResolution(void)
+      {
+      }
+
+      //! Acquire resources.
+      void
+      onResourceAcquisition(void)
+      { Insert short task description here.
+      void
+      onResourceInitialization(void)
+      {
+      }
+
+      //! Release resources.
+      void
+      onResourceRelease(void)
+      {
+      }
+
+      //! Main loop.
+      void
+      onMain(void)
+      {
+        while (!stopping())
+        {
+          if (isActive())
+          {
+            if (m_counter <= m_args.limit)
+            {
+              spew("counter: %d", m_counter);
+            }
+            else
+            {
+              //m_counter = 0;
+              requestDeactivation();
+            }
+          }
+          waitForMessages(1.0);
+        }
+      }
+    };
+  } // namespace Activatable
+} // namespace Monitors
+
+DUNE_TASK
